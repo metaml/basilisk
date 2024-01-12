0df616b1b152e013cb79f3bf32a8fadd9879f0fb
metaml <metaml@gmail.com>
2024-01-11 19:06:44 -0500

    wip: fixing stupid swig bugs

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@
 
 export SHELL := $(shell type --path bash)
 export PYTHONPATH := $(shell pwd)/dist3:$(shell pwd)/src:${PYTHONPATH}
-
-init: ## pip install conan
-	pip install conan==1.59.0
+ifeq ($(shell uname -s),Darwin)
+export LD = /usr/bin/clang
+endif
 
 build: ## build basilisk
 	python3 conanfile.py
@@ -18,8 +18,23 @@ test-dist3: ## test dist3
 test-src: ## test src
 	cd src && pytest --verbosity=1 --numprocesses=0
 
+clean: ## clean
+	rm -rf dist3
+
+clobber: clean ## clobber
+	rm -rf ~/.conan
+
+deps: install-xcode install-conan ## install deps (run this first)
+
+install-xcode: ## install xcode developer tools
+	-@sudo xcode-select --install
+
+install-conan: ## install conan
+	pip3 install wheel 'conan<2.0'
+
 help: ## help
 	-@grep --extended-regexp '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) \
 	| sed 's/^Makefile://1' \
 	| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-18s\033[0m %s\n", $$1, $$2}'
-
+	xcrun --find clang
+	xcrun --find ld
--- flake.nix
+++ flake.nix
@@ -18,31 +18,31 @@
             bashInteractive
             cmake
             cmakeCurses
-            conan
+            #conan
             coreutils
             findutils
-            gcc
             git
             gnugrep
             gnumake
             gnused
             python3
             python3.pkgs.colorama
-            python3.pkgs.pillow
             python3.pkgs.matplotlib
             python3.pkgs.numpy
             python3.pkgs.pandas
             python3.pkgs.parse
+            python3.pkgs.pillow
             python3.pkgs.pip
             python3.pkgs.pytest
             python3.pkgs.pytest-xdist
             python3.pkgs.setuptools
             python3.pkgs.tkinter
             python3.pkgs.tqdm
-            python3.pkgs.wheel
+            python3.pkgs.urllib3
             python3.pkgs.virtualenv
+            python3.pkgs.wheel
             sourceHighlight
-            swig
+            swig4
             watchexec
             zlib
           ];
@@ -50,9 +50,9 @@
           shellHook = ''
             export SHELL=$BASH
             export LANG=en_US.UTF-8
-            export PS1="basi|$PS1"
-            #virtualenv basilisk
-            #source basilisk/bin/activate
+            export PS1="nix|$PS1"
+            python3 -m venv .venv
+            source .venv/bin/activate
           '';
         };
 
